242889f9ca4b10a45648d4b35c6431b87459a9fe
Kaio Vitor Santos Queiroz <123708201+Kaio-0708@users.noreply.github.com>
2024-01-22 22:21:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+
+while ( chute != numeroSecreto) {
+    chute =  prompt(`escolha um número entre 1 e ${numeroMaximo}`);
+
+    if (chute == numeroSecreto) {
+       break;
+    }  else {
+        if(chute > numeroSecreto){
+            alert(` O número secreto é menor que ${chute}`);        
+        }
+        else{
+            alert(`O número secreto é maior que ${chute}`);
+        }
+          tentativas = tentativas + 1;
+          tentativas++;
+   
+    } 
+
+    
+}
+
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);   
+
+
+/* modo díficil da etapa de cima
+if(tentativas > 1){
+    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+   }
+   else{
+    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+   } 
+*/
+
